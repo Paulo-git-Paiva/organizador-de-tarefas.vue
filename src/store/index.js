486e1551dde7b25684e41728f2b0c9098d201cf7
Paulo Paiva <paulo.h.paiva.c@gmail.com>
2021-12-07 14:45:15 -0500

    project submission

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    tarefas: [
+      { id: 1, titulo: "Ir ao mercado", concluido: false },
+      { id: 2, titulo: "Compra ração", concluido: false },
+    ],
+  },
+  mutations: {
+    adicionaTarefa(state, titulo) {
+      if (titulo) {
+        state.tarefas.push({
+          id: new Date().getTime(),
+          titulo,
+          comcluido: false,
+        });
+      }
+    },
+    removeTarefa(state, id) {
+      state.tarefas = state.tarefas.filter((tarefa) => tarefa.id !== id);
+    },
+  },
+  actions: {},
+  modules: {},
+});
